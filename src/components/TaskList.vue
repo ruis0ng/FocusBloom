14dbcc07a0fd76121f9e90ca24f1c8e63747bc4b
Raygun999 <mssjsr@126.com>
2024-11-09 20:29:00 +0100

    added a list for displaying the task

---
+++ src/components/TaskList.vue
@@ -0,0 +1,29 @@
+<template>
+<ul class="mt-4 space-y-2">
+    <li v-for="(task, index) in tasks" :key="index" class="flex items-center">
+        <input type="checkbox" v-model="task.completed" @change="updateTaskCompletion" class="mr-2" />
+        <label :class="{ 'line-through': task.completed }">{{ task.title }}</label>
+    </li>
+</ul>
+</template>
+
+  
+<script>
+export default {
+    props: {
+        tasks: Array
+    },
+    methods: {
+        updateTaskCompletion() {
+            this.$emit("task-completed");
+        }
+    }
+};
+</script>
+
+  
+<style scoped>
+.line-through {
+    text-decoration: line-through;
+}
+</style>
