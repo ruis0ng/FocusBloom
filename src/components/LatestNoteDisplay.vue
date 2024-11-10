56c752b9e911779feaaeb35805cad6edf2d74185
Raygun999 <mssjsr@126.com>
2024-11-10 14:58:47 +0100

    format changes in latest resumption notes

--- src/components/LatestNoteDisplay.vue
+++ src/components/LatestNoteDisplay.vue
@@ -1,7 +1,8 @@
 <template>
     <div v-if="latestNote" class="mt-4 text-gray-700">
-      <h3 class="text-lg font-semibold">Latest Note</h3>
-      <p>{{ latestNote }}</p>
+      <h3>I want to remind you of the followings: </h3>
+      <p class="text-lg font-semibold">{{ latestNote }}</p>
+      <h3>I hope it can help you resume your task</h3>
     </div>
   </template>
   
