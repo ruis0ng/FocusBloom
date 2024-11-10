84c844658c7d8c2eaee9a49ae00101caeca8c4cd
Raygun999 <mssjsr@126.com>
2024-11-10 15:11:46 +0100

    the progressBarView fixed, the progress bar mode works fine.

--- src/views/ProgressBarView.vue
+++ src/views/ProgressBarView.vue
@@ -1,68 +1,106 @@
 <template>
 <div class="container mx-auto p-6">
-    <!-- Welcome Message -->
+    <!-- Display a welcome message for my users -->
     <WelcomeMessage />
 
-    <!-- Project Goal Input -->
-    <div v-if="!goal">
-        <ProjectGoalInput @update:goal="updateGoal" />
+    <!-- Text field for the user to set their goal (one goal for one time) -->
+    <ProjectGoalInput :goal="goal" @set-goal="setGoal" />
+
+    <div v-if="goal">
+        <ProjectGoalDisplay :goal="goal" />
+        <!-- Text field for the user to set the tasks for the goal -->
+        <TaskInput v-if="goal" @add-task="addTask" />
+        <!-- Display the checklist of the tasks -->
+        <TaskList :tasks="tasks" @update-progress="calculateProgress" @delete-task="deleteTask" />
+        <!-- Display the progress Bar -->
+        <ProgressBar v-if="tasks.length > 0" :progress="progress" />
+        <!-- Display the current task -->
+        <CurrentTaskDisplay :currentTask="currentTask" />
+        <!-- Resumption Notes -->
+        <NotesInput @add-note="addNote" />        
+        <LatestNoteDisplay :latestNote="latestNote" />
     </div>
+    <!-- Display their current goal for this time -->
 
-    <!-- Display Goal and Task Input -->
-    <div v-else>
-        <h2 class="text-xl font-semibold text-center mt-4">Your Project Goal: {{ goal }}</h2>
-
-        <!-- Task Input -->
-        <TaskInput @add-task="addTask" />
-
-        <!-- Task List -->
-        <TaskList :tasks="tasks" @task-completed="updateProgress" />
-
-        <!-- Progress Bar -->
-        <ProgressBar :progress="progress" />
-    </div>
 </div>
 </template>
 
 <script>
 import WelcomeMessage from '../components/WelcomeMessage.vue';
 import ProjectGoalInput from '../components/ProjectGoalInput.vue';
+import ProjectGoalDisplay from '../components/ProjectGoalDisplay.vue';
 import TaskInput from '../components/TaskInput.vue';
 import TaskList from '../components/TaskList.vue';
-import ProgressBar from '../components/ProgressBar.vue';
+import ProgressBar from '../components/ProgressBarComponents/ProgressBar.vue';
+import CurrentTaskDisplay from '../components/CurrentTaskDisplay.vue';
+import NotesInput from '../components/NotesInput.vue';
+import LatestNoteDisplay from '../components/LatestNoteDisplay.vue';
 
 export default {
     components: {
         WelcomeMessage,
         ProjectGoalInput,
+        ProjectGoalDisplay,
         TaskInput,
         TaskList,
-        ProgressBar
-
+        ProgressBar,
+        CurrentTaskDisplay,
+        NotesInput,
+        LatestNoteDisplay
     },
     data() {
         return {
             goal: '',
             tasks: [],
-            progress: 0
+            progress: 0,
+            currentTask: null,
+            latestNote: ''
         };
     },
     methods: {
-        updateGoal(goal) {
+        setGoal(goal) {
             this.goal = goal;
         },
-        addTask(task) {
-            this.tasks.push({
-                title: task,
+        addTask(taskName) {
+            const task = {
+                name: taskName,
                 completed: false
-            });
-            this.updateProgress();
+            };
+            this.tasks.push(task);
+            this.calculateProgress();
+            this.updateCurrentTask();
         },
-        updateProgress() {
-            const completedTasks = this.tasks.filter(task => task.completed).length;
-            this.progress = this.tasks.length > 0 ?
-                Math.round((completedTasks / this.tasks.length) * 100) : 0; //compute a rounded result about completeness of the goal
+        deleteTask(index) {
+            this.tasks.splice(index, 1);
+            this.calculateProgress();
+            this.updateCurrentTask();
+        },
+        calculateProgress() {
+            if (this.tasks.length === 0) {
+                this.progress = 0;
+            } else {
+                const completedTasks = this.tasks.filter(task => task.completed).length;
+                this.progress = (completedTasks / this.tasks.length) * 100;
+            }
+        },
+        updateCurrentTask() {
+            this.currentTask = this.tasks.find(task => !task.completed) || null;
+        },
+        addNote(note) {
+            this.latestNote = note;
+        }
+    },
+    watch: {
+        tasks: {
+            deep: true,
+            handler() {
+                this.calculateProgress();
+                this.updateCurrentTask();
+            }
         }
     }
 };
 </script>
+
+<style scoped>
+</style>
