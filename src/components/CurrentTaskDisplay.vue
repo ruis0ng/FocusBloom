613f94256bf35cbb2537d9b55e625447853720b7
Raygun999 <mssjsr@126.com>
2024-11-10 14:30:57 +0100

    Changes in current task display component

---
+++ src/components/CurrentTaskDisplay.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="currentTask" class="mt-4">
+      <h3 class="text-lg font-semibold">Your Current Task is:</h3>
+      <p class="text-gray-700">{{ currentTask.name }}</p>
+    </div>
+    <div v-else class="mt-4 text-gray-700">
+      <h3 class="text-lg font-semibold">Your Current Task is:</h3>
+      <p>All tasks completed! Congratulations!🎉</p>
+    </div>
+  </template>
+
+<script>
+export default {
+  props: {
+    currentTask: {
+      type: Object,
+      default: null
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
